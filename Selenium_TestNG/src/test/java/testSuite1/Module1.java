a07effd110e2123fb616b9865a32b33c33afcd11
anjujohn <anjujohn@anjus-air>
2023-12-26 17:48:07 -0500

    Commit1

---
+++ Selenium_TestNG/src/test/java/testSuite1/Module1.java
@@ -0,0 +1,25 @@
+package testSuite1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+//import org.testng.annotations.Test;
+
+public class Module1 {
+	
+	@Test
+	public void test001() {
+		System.out.println("Test case 001");
+	}
+	
+	@Test
+	public void test002() {
+		System.out.println("Test case 002");
+	}
+	
+	@Test
+	public void test003() {
+		System.out.println("Test case 003");
+	}
+
+}
---
+++ Selenium_TestNG/src/test/java/testSuite1/Module2.java
@@ -0,0 +1,22 @@
+package testSuite1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+ public class Module2 {
+	
+	@Test
+	public void test001() {
+		System.out.println("Test case 001");
+	}
+	
+	@Test
+	public void test002() {
+		System.out.println("Test case 002");
+	}
+	
+	@Test
+	public void test003() {
+		System.out.println("Test case 003");
+	}
+}
